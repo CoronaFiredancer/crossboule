b56f7d177b4c2ecc1ccf39842fee5841826b74cc
Carsten <cwo@visiolink.com>
2014-04-23 23:41:22 +0200

    working with sqlite

---
+++ XBoule/XBoule/DataAccess/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sqlite;
+
+namespace XBoule.DataAccess
+{
+	class Database
+	{
+		private async void CreateDatabase() {
+			var db = new Sqlite.Database();
+
+		}
+	}
+}
