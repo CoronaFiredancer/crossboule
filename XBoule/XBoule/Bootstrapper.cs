71d7fae458747e3d4f9f288970b0f665630c86ad
Carsten <cwo@visiolink.com>
2014-04-23 20:56:46 +0200

    bootstapper set up cleaning App.cs next

---
+++ XBoule/XBoule/Bootstrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using XBoule.ViewModels;
+
+namespace XBoule
+{
+	public class Bootstrapper : PhoneBootstrapper
+	{
+		private PhoneContainer container;
+
+		protected override void Configure()
+		{
+			container = new PhoneContainer();
+			container.RegisterPhoneServices(RootFrame);
+			container.PerRequest<MainViewModel>();
+		}
+		protected override object GetInstance(Type service, string key)
+		{
+			return container.GetInstance(service, key);
+		}
+
+		protected override IEnumerable<object> GetAllInstances(Type service)
+		{
+			return container.GetAllInstances(service);
+		}
+
+		protected override void BuildUp(object instance)
+		{
+			container.BuildUp(instance);
+		}
+	}
+}
